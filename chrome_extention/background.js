16615b8b543f7edae7388baa54052cb8b128a78f
nicest414 <kechapoko@gmail.com>
2025-04-03 15:00:46 +0900

    実際にカートの中身と画像を取得できるようになった

--- chrome_extention/background.js
+++ chrome_extention/background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed!');
-  });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "download") {
+        chrome.downloads.download({
+            url: message.url,
+            filename: "amazon_images/" + message.filename,
+            saveAs: false  // ユーザーに確認ダイアログを出さずに自動保存
+        });
+    }
+});
+
+// 特に処理なし（Manifest V3 では Service Worker が必要）
